d5a7ddd20c7d43388561af0547e1f1c3432fa91b
marixc <marixc@users.noreply.github.com>
2024-03-21 13:03:04 +0100

    feat(nvim): toggle format on save

--- config/nvim/.config/nvim/lua/plugins/conform.lua
+++ config/nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,24 +1,47 @@
 return {
-	-- formatter
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
-			},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					javascript = { { "prettierd", "prettier" } },
+				},
+				format_on_save = function(bufnr)
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
+					return { timeout_ms = 500, lsp_fallback = true }
+				end,
+			})
 
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
+
+			-- format with key mapping
 			vim.keymap.set("n", "<leader>df", function()
 				conform.format({
 					timeout_ms = 500,
 					lsp_fallback = true,
 				})
-			end),
-		})
-	end,
+			end)
+		end,
+	},
 }
