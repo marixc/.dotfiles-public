6ad76087e1648d67d1020c4978fe962053caa643
marixc <marixc@users.noreply.github.com>
2024-03-29 17:44:54 +0100

    feat(nvim): harpoon mapping for split kb

--- config/nvim/.config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,6 +9,7 @@ return {
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
 		vim.keymap.set("n", "<leader>1", function()
 			ui.nav_file(1)
 		end)
@@ -22,6 +23,23 @@ return {
 			ui.nav_file(4)
 		end)
 		vim.keymap.set("n", "<leader>5", function()
+			ui.nav_file(5)
+		end)
+
+		-- for faster accs.
+		vim.keymap.set("n", "sy", function()
+			ui.nav_file(1)
+		end)
+		vim.keymap.set("n", "su", function()
+			ui.nav_file(2)
+		end)
+		vim.keymap.set("n", "si", function()
+			ui.nav_file(3)
+		end)
+		vim.keymap.set("n", "so", function()
+			ui.nav_file(4)
+		end)
+		vim.keymap.set("n", "sp", function()
 			ui.nav_file(5)
 		end)
 	end,
