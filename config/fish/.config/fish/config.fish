ca018e6523e5fa0d63e0593c4bac590df1a1f968
marixc <marixc@users.noreply.github.com>
2024-04-12 16:27:47 +0200

    feat(fish): doom path

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	set fish_greeting #surpresses fish's intro message
+    set -x PATH $HOME/.config/emacs/bin $PATH
     source $HOME/.config/shell/aliasrc
     bind \cf "tmux-sessionizer"
     bind \cg "tmux-sessionizer-max"
