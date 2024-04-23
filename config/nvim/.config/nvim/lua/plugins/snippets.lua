e306c172a4e8a762733e0a35cdc2fe5698e2b750
marixc <marixc@users.noreply.github.com>
2024-04-23 21:27:25 +0200

    feat(nvim): snippets

---
+++ config/nvim/.config/nvim/lua/plugins/snippets.lua
@@ -0,0 +1,11 @@
+return {
+	"L3MON4D3/LuaSnip", -- snippet engine
+	init = function()
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local t = ls.text_node
+		local i = ls.insert_node
+
+		-- custom snippet can go here
+	end,
+}
